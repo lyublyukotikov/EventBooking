8b21fe5e7aa14fbb530f122a606e2fee9b965a49
Aleksey Mikhailov <karpenochkanv@gmail.com>
2024-03-02 22:45:24 +0300

    change database url and add url to .env

--- src/server/connect_db/db.connect.js
+++ src/server/connect_db/db.connect.js
@@ -1,19 +1,23 @@
 // db.connect.js
 import { Sequelize } from "sequelize";
+// импортируем переменную окружения
+import { config } from 'dotenv';
+config();
 
-const sequelize = new Sequelize('ovoshebaza_db', 'postgres', '12345678', {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    },
+  }
+);
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log("Connected to PosgreSQL database");
+    console.log("Connected to PostgreSQL database");
   })
   .catch((error) => {
     console.log("Unable to connect to database:", error);
