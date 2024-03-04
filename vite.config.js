7020610ea0b11c7a1ea4674a6464c6d294e1956a
Aleksey Mikhailov <karpenochkanv@gmail.com>
2024-03-05 00:34:20 +0300

    change vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import envCompatible from 'vite-plugin-env-compatible';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), envCompatible()],
+  server: {
+    host: '0.0.0.0',
+  },
 });
