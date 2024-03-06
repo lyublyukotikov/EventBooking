df9d38f6bc681297f5daf8d7d9d66b6e574a0af3
Aleksey Mikhailov <karpenochkanv@gmail.com>
2024-03-06 18:38:58 +0300

    change url

--- src/server/controllers/user-controller.js
+++ src/server/controllers/user-controller.js
@@ -11,6 +11,7 @@ config();
 // ссылка на клиент
 // eslint-disable-next-line no-undef
 const CLIENT_URL = process.env.CLIENT_URL;
+
 class UserController {
   // регистрация
   async registration(req, res, next) {
